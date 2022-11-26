7d111691a6f353eb6a50329b14af4c15ae496f4e
kadefi <kadefi.money@gmail.com>
2022-11-27 01:39:50 +0800

    add timeout and logs

--- src/materialViewUpdate.js
+++ src/materialViewUpdate.js
@@ -1,8 +1,10 @@
 
 const materialViewUpdate = async (client) => {
   try {
+    console.log("starting refresh");
     await client.query(`REFRESH MATERIALIZED VIEW CONCURRENTLY kaddex_hl_view;`)
     await client.query(`REFRESH MATERIALIZED VIEW CONCURRENTLY kdswap_hl_view;`)
+    console.log("ending refresh");
   } catch (e) {
     console.log(e);
     console.log("error happaend");
