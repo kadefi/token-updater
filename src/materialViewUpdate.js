b7b633a4d20c74d4c230d259ccb0818e6db93b7e
kadefi <kadefi.money@gmail.com>
2023-01-08 23:34:12 +0800

    update material for kda

--- src/materialViewUpdate.js
+++ src/materialViewUpdate.js
@@ -4,6 +4,8 @@ const materialViewUpdate = async (client) => {
     console.log("starting refresh");
     await client.query(`REFRESH MATERIALIZED VIEW CONCURRENTLY kaddex_hl_view;`)
     await client.query(`REFRESH MATERIALIZED VIEW CONCURRENTLY kdswap_hl_view;`)
+    await client.query(`REFRESH MATERIALIZED VIEW CONCURRENTLY kdswap_hl_view_kda;`)
+    await client.query(`REFRESH MATERIALIZED VIEW CONCURRENTLY kdswap_hl_view_kda;`)
     console.log("ending refresh");
   } catch (e) {
     console.log(e);
