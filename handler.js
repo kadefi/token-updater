cb1b15a426816b6de8e51fcdd052945e08e6dfd3
kadefi <kadefi.money@gmail.com>
2023-03-03 23:49:44 +0800

    client per function

--- handler.js
+++ handler.js
@@ -1,32 +1,45 @@
 "use strict";
 
 const { Client, Pool } = require("pg");
-const client = new Client();
-client.connect();
 
 const updatePrice = require("./src/updatePrice");
 const updatePriceHandler = async (event) => {
+  const client = new Client();
+  client.connect();
+
   await updatePrice(client);
 };
 
 const tokenUpdate = require("./src/tokenUpdate");
 const updateTokenHandler = async (event) => {
+  const client = new Client();
+  client.connect();
+
   await tokenUpdate(client);
 };
 
 const candleUpdate = require("./src/candleUpdate");
 const candleUpdateHandler = async (event) => {
+  const client = new Client();
+  client.connect();
+
   const { dex, base } = event;
   await candleUpdate(dex, client, base);
 };
 
 const materialViewUpdate = require("./src/materialViewUpdate");
 const materialViewUpdateHandler = async (event) => {
+  const client = new Client();
+  client.connect();
+
   await materialViewUpdate(client);
 };
 
 const hourBarsUpdate = require("./src/hourBarsUpdate");
 const hourBarsUpdateHandler = async (event) => {
+  const client = new Client();
+  client.connect();
+
   const { dex, base } = event;
   await hourBarsUpdate(dex, client, base);
 };
